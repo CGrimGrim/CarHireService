943c73a516e1e4180e342559eb04a35640c27dd9
RYAN DUTTON <RYAN.DUTTON.DWP@GMAIL.COM>
2015-06-10 09:45:58 +0100

    Data Link complete

--- src/hire/car/a2b/dao/DataLink.java
+++ src/hire/car/a2b/dao/DataLink.java
@@ -1,49 +1,111 @@
 package hire.car.a2b.dao;
 
-import java.sql.*;
-import java.time.LocalDate;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DataLink implements IDataLink {
-	
-	public DataLink(){
-		
+
+	Connection connection = null;
+
+	public DataLink() {
+		try {
+			Class.forName("com.mysql.JDBC.Driver");
+		} catch (Exception e) {
+		}
 	}
 
 	@Override
-	public void insertCustomerDetails(String username, String password, String name,
-			LocalDate DOB, String[] address, int contactNumber) {
-		// TODO Auto-generated method stub
+	public void insertCustomerDetails(String username, String password,
+			String name, Date DOB, String[] address_line_1,
+			String[] address_line_2, String[] city, String[] postcode,
+			int contactNumber) {
+		try {
 
+			PreparedStatement statement = connection
+					.prepareStatement("call insert_customer(?,?,?,?,?,?,?,?,?)");
+			statement.setString(1, username);
+			statement.setString(2, password);
+			statement.setString(3, name);
+			statement.setDate(4, DOB);
+			statement.setString(5, address_line_1[0]);
+			statement.setString(6, address_line_2[1]);
+			statement.setString(7, city[2]);
+			statement.setString(8, postcode[3]);
+			statement.setInt(9, contactNumber);
+			statement.execute();
+		} catch (SQLException e) {
+		}
 	}
 
 	@Override
 	public ResultSet getVehicleGroups() {
-		// TODO Auto-generated method stub
-		return null;
+		ResultSet rs = null;
+		try {
+			PreparedStatement statement = connection
+					.prepareStatement("call list_vehicle_groups();");
+			rs = statement.executeQuery();
+		} catch (SQLException e) {
+		}
+
+		return rs;
+
 	}
 
 	@Override
 	public ResultSet getAllCarsOnHire() {
-		// TODO Auto-generated method stub
-		return null;
+		ResultSet rs = null;
+		try {
+			return connection.prepareStatement(
+					"SELECT * FROM vehicles WHERE is_on_hire= true")
+					.executeQuery();
+		} catch (SQLException e) {
+		}
+		return rs;
 	}
 
 	@Override
 	public ResultSet getAllAvailableCars() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		ResultSet rs = null;
+		try {
+			return connection.prepareStatement(
+					"call list_vehicles_available();").executeQuery();
 
+		} catch (SQLException e) {
+		}
+		return rs;
+
+	}
 
 	@Override
 	public ResultSet getAllCustomerInvoices(int customerID) {
-		// TODO Auto-generated method stub
-		return null;
+		ResultSet rs = null;
+		try {
+			PreparedStatement statement = connection
+					.prepareStatement("call list_customer_invoices(?);");
+			statement.setInt(1, customerID);
+			rs = statement.executeQuery();
+		} catch (SQLException e) {
+		}
+
+		return rs;
 	}
-	
+
 	@Override
-	public ResultSet getUserInfo(String username){
-		return null;
+	public ResultSet getUserInfo(String username) {
+		ResultSet rs = null;
+		try {
+			PreparedStatement statement = connection
+					.prepareStatement("SELECT * FROM customer WHERE uname=?");
+			statement.setString(1, username);
+			rs = statement.executeQuery();
+
+		} catch (SQLException e) {
+		}
+
+		return rs;
 	}
 
 }
